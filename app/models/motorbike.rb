dfbb1a1b7502ebbbe5e5de1fc5813a22e2cd492a
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-02 18:17:21 +0800

    added validations, added gem files, and added necessary medium files

--- app/models/motorbike.rb
+++ app/models/motorbike.rb
@@ -1,3 +1,18 @@
 class Motorbike < ApplicationRecord
+  has_many :reviews
+  has_many :bookings
   belongs_to :user
+
+  validates :name, presence: true
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
