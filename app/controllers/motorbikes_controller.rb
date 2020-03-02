fe6ccc3070c18cd400c3d3fd98a4ecc7872ea54f
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-02 22:26:12 +0800

    added the required functions for each methods in motorbike controller

--- app/controllers/motorbikes_controller.rb
+++ app/controllers/motorbikes_controller.rb
@@ -1,22 +1,43 @@
 class MotorbikesController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @motorbikes = Motorbike.all
   end
 
   def show
+    @motorbike = Motorbike.find(params[:id])
+  end
+
+  def new
+    @motorbike = Motorbike.new
+  end
+
+  def create
+    @motorbike = Motorbike.new(motorbike_params)
+    if @motorbike.save
+      redirect_to motorbike_path(@motorbike)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @motorbike = Motorbike.find(params[:id])
   end
 
   def update
+    @motorbike = Motorbike.find(params[:id])
+    @motorbike.update(motorbike_params)
+    redirect_to @motorbike
   end
 
   def destroy
+    @motorbike = Motorbike.find(params[:id])
+    @motorbike.destroy
+  end
+
+  private
+
+  def motorbike_params
+    params.require(:motorbike).permit(:name, :make, :model, :year, :description, :address)
   end
 end
