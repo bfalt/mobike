b0b83ece8a929867991f0fa075e9bb9b0d9f7b5d
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-05 19:59:47 +0800

    added the statements to create a review once the booking is finished, emplemented pundit so only the user of the booking can leave a review.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :description, length: { minimum: 10 }
-  validates :stars, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
