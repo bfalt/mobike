97d644146e613b1624042f3e6e4a1395815d6f4f
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-06 13:27:57 +0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_025933) do
+ActiveRecord::Schema.define(version: 2020_03_06_045848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_025933) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_price"
     t.index ["motorbike_id"], name: "index_bookings_on_motorbike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
