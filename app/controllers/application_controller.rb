dc275e50625853c3f957cfa9b9ae089526d71f8f
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-06 16:32:08 +0800

    user can upload an image to their profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -12,6 +14,10 @@ class ApplicationController < ActionController::Base
   # end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
