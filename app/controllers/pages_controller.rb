c8842df10a08987575f9e65cf8cda1c563fd01a7
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-06 15:46:29 +0800

    added styles to the dashboard, shows total price added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
   end
+
   def dashboard
     @motorbikes = current_user.motorbikes
     @bookings = current_user.bookings
-
   end
 end
 
